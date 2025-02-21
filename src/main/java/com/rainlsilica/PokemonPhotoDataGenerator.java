67aa35964857008fa99652de0d96a4c2459f899c
RainlSilica <3383686958@qq.com>
2025-02-21 21:54:57 +0800

    宝可梦重铸照片创建

---
+++ src/main/java/com/rainlsilica/PokemonPhotoDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rainlsilica;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PokemonPhotoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
